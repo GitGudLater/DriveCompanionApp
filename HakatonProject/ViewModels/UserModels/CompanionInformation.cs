52f1ded47b27702e655af9ac8f7d1fc9d683bfa9
Andrey <slendernet@@gmail.com>
2019-10-01 16:05:27 +0300

    Updated View Model structure and added Requests models

---
+++ HakatonProject/ViewModels/UserModels/CompanionInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HakatonProject.ViewModels.UserModels
+{
+    public class CompanionInformation
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string RequestDescription { get; set; }
+
+        public string UserDescription { get; set; }
+
+    }
+}
