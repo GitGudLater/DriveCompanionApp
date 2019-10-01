acd7247d26ec13825d276f75f485f0df146e71bf
Andrey <slendernet@@gmail.com>
2019-10-01 15:31:17 +0300

    Updated basis View Model structure

---
+++ HakatonProject/ViewModels/UserModels/UserInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HakatonProject.ViewModels.UserModels
+{
+    public class UserInformation
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Description { get; set; }
+
+        public string CarRegistered { get; set; }
+
+        public List<string> CarSubscribed { get; set; }
+    }
+}
