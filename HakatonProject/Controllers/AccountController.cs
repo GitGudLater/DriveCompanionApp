b4ae14ed1dbb8139c7b8413d6eb58d8c61064500
Andrey <slendernet@@gmail.com>
2019-11-03 10:45:19 +0300

    basis functions account controler - nedeed in refactoring

--- HakatonProject/Controllers/AccountController.cs
+++ HakatonProject/Controllers/AccountController.cs
@@ -4,12 +4,92 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using HakatonProject.EntityModels;
+using HakatonProject.ViewModels.UserModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
+/// <summary>
+///Make it in Interface and service realization. Add request for companioninformation
+/// </summary>
 namespace HakatonProject.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private CompanionContext db;
+
+        public AccountController(CompanionContext context)
+        {
+            db = context;
+        }
+
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([FromBody]Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(model.Email); // аутентификация
+
+                    return Ok(model);
+                }
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+                return Ok(model);
+            }
+            else
+                return BadRequest(ModelState);
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Register([FromBody]Registration model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    // добавляем пользователя в бд
+                    db.Users.Add(new User { Email = model.Email, Password = model.Password });
+                    await db.SaveChangesAsync();
+
+                    await Authenticate(model.Email); // аутентификация
+
+                    return Ok(model);
+                }
+                else
+                    return BadRequest(ModelState);
+            }
+            return BadRequest(ModelState);
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
     }
 }
